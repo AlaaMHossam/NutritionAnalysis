139c87f6ede8caccc911e4a6aa6dca823d6bcafa
Alaa Hossam <alaa.m.hossam@gmail.com>
2021-05-31 21:47:53 +0200

    Setting up MainActivity.kt

---
+++ app/src/main/java/com/alaahossam/nutritionanalysis/presentation/IngredientInputFragment.kt
@@ -0,0 +1,12 @@
+package com.alaahossam.nutritionanalysis.presentation
+
+import app.hatrick.core.base.BaseFragment
+import com.alaahossam.nutritionanalysis.R
+import com.alaahossam.nutritionanalysis.databinding.FragmentIngredientInputBinding
+
+class IngredientInputFragment : BaseFragment<FragmentIngredientInputBinding>() {
+
+    override fun getLayoutResourceId(): Int = R.layout.fragment_ingredient_input
+
+
+}
---
+++ app/src/main/java/com/alaahossam/nutritionanalysis/presentation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.alaahossam.nutritionanalysis.presentation
+
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.alaahossam.core.base.BaseActivity
+import com.alaahossam.nutritionanalysis.R
+import com.alaahossam.nutritionanalysis.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override fun getLayoutResourceId(): Int = R.layout.activity_main
+
+    override fun onBindData(binding: ActivityMainBinding) {
+        super.onBindData(binding)
+
+        val navController = findNavController(R.id.fragment)
+        binding.toolbar.setupWithNavController(navController)
+    }
+}
